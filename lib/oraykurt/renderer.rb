fd40791059160193d0010730de729213898b2e69
Oray KURT <oraykt@gmail.com>
2021-02-04 12:40:01 +0100

    gem renderer class updated

--- lib/oraykurt/renderer.rb
+++ lib/oraykurt/renderer.rb
@@ -1,5 +1,7 @@
-class Renderer
-  def self.copyright
-    "&copy; #{Time.now.year}&nbsp;|&nbsp;<b>Oray KURT</b>&nbsp;All rights reserved".html_safe
+module Oraykurt
+  class Renderer
+    def self.copyright
+      "&copy; #{Time.now.year}&nbsp;|&nbsp;<b>Oray KURT</b>&nbsp;All rights reserved".html_safe
+    end
   end
 end
