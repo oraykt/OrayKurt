dc2ef9f19b259f5945c9c3310327f4718df0693d
Oray KURT <oraykt@gmail.com>
2021-02-04 21:57:53 +0100

    Small changes on html code

--- lib/oraykurt/renderer.rb
+++ lib/oraykurt/renderer.rb
@@ -1,7 +1,7 @@
 module Oraykurt
   class Renderer
     def self.copyright
-      "&copy; #{Time.now.year}&nbsp;|&nbsp;<b>Oray KURT</b>&nbsp;All rights reserved".html_safe
+      "&copy; #{Time.now.year}&nbsp;|&nbsp;<b>Oray KURT</b>&nbsp;|&nbsp;All rights reserved".html_safe
     end
   end
 end
