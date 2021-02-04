6d0cf80712b0fcf84a67589a3e6cb2d5fe7d78b7
Oray KURT <oraykt@gmail.com>
2021-02-04 12:33:37 +0100

    Initial Commit for custom gem

---
+++ lib/oraykurt.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "oraykurt/version"
+require_relative "oraykurt/renderer"
+module Oraykurt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/oraykurt/renderer.rb
@@ -0,0 +1,5 @@
+class Renderer
+  def self.copyright
+    "&copy; #{Time.now.year}&nbsp;|&nbsp;<b>Oray KURT</b>&nbsp;All rights reserved".html_safe
+  end
+end
